156030e9e64c71711c40d1a22bfa4ecb7b44644c
Marvin Raydel Rios Aguilar <ia.marvinrios@gmail.com>
2026-03-11 18:25:27 -0600

    Update

--- js/script.js
+++ js/script.js
@@ -1,3 +1,143 @@
+let maxLeft;
+let maxTop;
+const minLeft = 0;
+const minTop = 0;
+let timeDelta;
+let img = [
+    "media/img/arboles/arbol1.png",
+    "media/img/arboles/arbol2.png",
+    "media/img/arboles/arbol3.png",
+    "media/img/arboles/arbol4.png"
+]
+
+var originalX;
+var originalY;
+
+window.onload = function() {
+    document.onmousedown = startDrag;
+    document.onmouseup = stopDrag;
+}
+
+function sensorClick(){
+    if(Date.now() - timeDelta < 150){
+        createPopup();
+    }
+}
+
+function createPopup(parent){
+    let p = document.getElementById("popup");
+
+    if (p){
+        p.parentNode.removeChild(p);
+    }
+
+    let popup = document.createElement("div");
+    popup.id = "popup";
+    popup.className = "popup";
+    popup.style.top = parent.y - 110 + "px";
+    popup.style.left = parent.x - 75 + "px";
+
+    let text = document.createElement("span");
+    text.textContent = parent.id;
+    popup.appendChild(text);
+
+    var map = document.getElementsByClassName("map");
+    map.appendChild(popup);
+}
+
 function baseOnLoad(){
-    return;
+    var map = document.getElementsByClassName("map")[0];
+    let base = document.getElementsByClassName("base")[0];
+    maxLeft = base.width - 50;
+    maxTop = base.top - 50;
+
+    for (let i = 0; i < 6; i++){
+        let sensor = document.createElement("img");
+        sensor.src = img[i % img.length];
+        sensor.alt = i;
+        sensor.id = id;
+        sensor.draggable = true;
+        sensor.classList.add("sensor");
+        sensor.classList.add("dragme");
+        sensor.style.left = `${Math.floor(Math.random() * 900)}px`;
+        sensor.style.top = `${Math.floor(Math.random() * 500)}px`;
+        sensor.onclick = sensorClick;
+
+        let parent = document.getElementsByClassName("map")[0];
+        parent.appendChild(sensor);
+    }
+
+}
+
+function startDrag(){
+    timeDelta = Date.now();
+    if(!e){ // si no hay evento
+        var e = window.event;
+    }
+    if(e.preventDefault){
+        e.preventDefault
+    }
+
+    targ = e.target ? e.target : e.srcElement;
+    originalX = targ.style.left;
+    originalY = targ.style.top;
+
+    if (!targ.classList.contains("dragme")){
+        return;
+    }
+
+    offsetX = e.clientX;
+    offsetY = e.clientY;
+
+    coordX = e.parseInt(targ.style.left);
+    coordY = e.parseInt(targ.style.top);
+    drag = true;
+
+    document.onmousemove = dragDiv;
+    return false;
+}
+
+function dragDiv(e){
+    if(!drag){
+        return;
+    }
+    if(!e){
+        e = window.event;
+    }
+
+    let newLeft = coordX + e.clientX - offsetX;
+    if (newLeft < maxLeft && newLeft > minLeft){
+        targ.style.left = newLeft + "px";
+    }
+
+    let newTop = coordY + e.clientY - offsetY;
+    if (newTop < maxTop && newTop > minTop){
+        targ.style.top = newTop + "px";
+    }
+
+    return false;
+}
+
+function stopDrag(){
+    if (typeof drag == "undefined"){
+        return;
+    }
+
+    if (drag){
+
+        if (Date.now() - timeDelta > 150){
+
+            let p = document.getElementById("popup");
+            if (p){
+                p.parentNode.removeChild(p);
+            }
+
+        }
+        else{
+            targ.style.left = originalX;
+            targ.style.top = originalY;
+        }
+    }
+
+    drag = false;
 }
